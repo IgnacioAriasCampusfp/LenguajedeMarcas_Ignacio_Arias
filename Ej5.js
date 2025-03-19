b47865420c77f9e73234f1c17d4d4651b6a2ce7d
IgnacioAriasCampusfp <ignacio.ariastomas24@campusfp.es>
2025-03-19 12:00:24 +0100

    Update Ej5.js

---
+++ Ej5.js
@@ -0,0 +1,8 @@
+function primo() {
+    let n1 = parseInt(prompt("Dime el numero que quieres empezar"));
+    let n2 = parseInt(prompt("Dime donde quieres terminar"));
+    for (let index = n1; index < n2; index++) {
+        
+        
+    }
+}
